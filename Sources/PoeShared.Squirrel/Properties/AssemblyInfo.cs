6e6b4895a1fdb347465627fdec2adfc26119cfd8
Xab3r <mail.xaber@gmail.com>
2020-02-28 01:46:24 +0300

    Removed PoeShared.Squirrel (moved to shared modules)

--- Sources/PoeShared.Squirrel/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Windows;
-
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
